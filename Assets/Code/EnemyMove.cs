81b6a6e9847239c6079a4f00e61720324a926754
icyhealer <sangsangji2@gmail.com>
2024-05-28 17:35:44 +0900

    마나 드롭

--- Assets/Code/EnemyMove.cs
+++ Assets/Code/EnemyMove.cs
@@ -14,6 +14,7 @@ public class EnemyMove : MonoBehaviour
     public int maxHealth = 3; // 몬스터의 최대 체력
     private int currentHealth; // 현재 체력
     private bool isDead = false; // 몬스터가 죽었는지 여부를 나타내는 변수
+    public GameObject manaPrefab; // 마나 프리팹
     
     private void Start()
     {
@@ -44,8 +45,15 @@ public class EnemyMove : MonoBehaviour
         if (portalManager != null) // portalManager가 null이 아닌지 확인합니다.
         {
             // 죽음 처리 로직
-            portalManager.MonsterDied();// 포탈 매니저에 죽은 몬스터 수를 알림
+            portalManager.MonsterDied(); // 포탈 매니저에 죽은 몬스터 수를 알림
+
+            // 마나 드롭
+            if (manaPrefab != null)
+            {
+                Instantiate(manaPrefab, transform.position, Quaternion.identity);
+            }
             Destroy(gameObject); // 몬스터 오브젝트 파괴
+            
             // 몬스터가 죽었음을 알리고 상태를 변경함
             isDead = true;
         }
