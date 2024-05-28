05dfc57beba55229c0dcb9951f0985339d5643df
czyct0614 <czyct0614@gmail.com>
2024-05-28 20:58:34 +0900

    이어하기 버그 일부수정

--- Assets/Code/Roomcode.cs
+++ Assets/Code/Roomcode.cs
@@ -2,31 +2,31 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class RoomCode : MonoBehaviour
-{
+public class RoomCode : MonoBehaviour {
     public RoomBounds roomBounds;
+
     private GameObject playerObject;
 
-    // Start 메서드는 첫 프레임 업데이트 전에 호출됩니다.
-    void Start()
-    {
+    void Start() {
         playerObject = GameObject.FindGameObjectWithTag("Player");
     }
 
-    private void OnTriggerEnter2D(Collider2D other)
-    {
-        if (other.CompareTag("Player"))
-        {
+    void Update() {
+        if (playerObject == null) {
+            playerObject = GameObject.FindGameObjectWithTag("Player");
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D other) {
+        if (other.CompareTag("Player")) {
+            Debug.Log("Player entered room: " + roomBounds.roomName);
             CameraFollow.Instance.SetCurrentRoom(roomBounds);
         }
     }
 
-    private void OnTriggerExit2D(Collider2D other)
-    {
-        if (other.CompareTag("Player"))
-        {
-            if (CameraFollow.Instance.GetCurrentRoomName() == roomBounds.roomName)
-            {
+    private void OnTriggerExit2D(Collider2D other) {
+        if (other.CompareTag("Player")) {
+            if (CameraFollow.Instance.GetCurrentRoomName() == roomBounds.roomName) {
                 CameraFollow.Instance.ClearCurrentRoom();
             }
         }
