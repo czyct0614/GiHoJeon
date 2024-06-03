442d5175847d5c46bb2743c0fb71517d4df41e7c
czyct0614 <czyct0614@gmail.com>
2024-06-03 18:04:21 +0900

    코드 위치 변경+카메라 크기 변경+문여는 코드

--- Assets/Code/DoorCode.cs
+++ Assets/Code/DoorCode.cs
@@ -1,11 +1,18 @@
 using UnityEngine;
-
+using System.Collections;
+using System.Collections.Generic;
 public class Door : MonoBehaviour
 {
     public string keyItemName = "Key"; // 키 아이템의 이름
     public GameObject door; // 문 오브젝트
     GameObject itemwindow;
 
+    
+    public GameObject DoorAxis;
+    public float rotationSpeed;
+    private const float maxRotationValue = 90f;
+    private const float FLOAT_COMPARISON_VALUE = 2f;
+    private WaitForSeconds gatePassWaitTime = new WaitForSeconds(0.5f);
     void Start(){
         itemwindow = GameObject.FindGameObjectWithTag("ItemWindow");
     }
@@ -29,7 +36,36 @@ public class Door : MonoBehaviour
         // 문을 여는 로직을 여기에 구현합니다. 예: 문 오브젝트를 비활성화
         if (door != null)
         {
-            door.SetActive(false);
+            StartCoroutine(GateOpen());
         }
+    }
+
+    IEnumerator GateOpen()
+    {
+
+        while (true)
+        {
+            float DoorCurEulerZ = DoorAxis.transform.eulerAngles.z;
+            bool isCompleteOpen =_Approximately(DoorCurEulerZ, maxRotationValue);
+
+            if (isCompleteOpen)
+            {
+                break;
+            }
+
+            DoorAxis.transform.eulerAngles =
+            new Vector3(0f,0f, Mathf.LerpAngle( DoorCurEulerZ,
+                                                maxRotationValue, 
+                                                Time.deltaTime * rotationSpeed));
+            yield return null;
+        }
+    }
+
+    private bool _Approximately(float x, float y)
+    {
+        float absCalcValue = (x - y) > 0f ? x - y : y - x;
+        if (absCalcValue <= FLOAT_COMPARISON_VALUE)
+            return true;
+        return false;
     }
 }
