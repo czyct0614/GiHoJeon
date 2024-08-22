f3caf5e830bf6fb0b15542433ace92308d632288
chobi8993 <chobi8993@gmail.com>
2024-08-22 23:07:09 +0900

    Revert "간단정리"
    This reverts commit 9cfea5f6b2d242d432ea1eb8c89a68cd82a4f206.

--- Assets/Code/ScriptFinder.cs
+++ Assets/Code/ScriptFinder.cs
@@ -11,17 +11,17 @@ public class Script : MonoBehaviour
 
         if (null == instance)
         {
-            // 이 클래스 인스턴스가 탄생했을 때 전역변수 instance에 게임매니저 인스턴스가 담겨있지 않다면, 자신을 넣어준다.
+            //이 클래스 인스턴스가 탄생했을 때 전역변수 instance에 게임매니저 인스턴스가 담겨있지 않다면, 자신을 넣어준다.
             instance = this;
 
-            // 씬 전환이 되더라도 파괴되지 않게 한다.
-            // gameObject만으로도 이 스크립트가 컴포넌트로서 붙어있는 Hierarchy상의 게임오브젝트라는 뜻이지만, 
-            // 헷갈림 방지를 위해 this를 붙여주기도 한다.
+            //씬 전환이 되더라도 파괴되지 않게 한다.
+            //gameObject만으로도 이 스크립트가 컴포넌트로서 붙어있는 Hierarchy상의 게임오브젝트라는 뜻이지만, 
+            //헷갈림 방지를 위해 this를 붙여주기도 한다.
             DontDestroyOnLoad(this.gameObject);
         }
         else
         {
-            // 이미 전역변수인 instance에 인스턴스가 존재한다면 자신을 삭제해준다.
+            //이미 전역변수인 instance에 인스턴스가 존재한다면 자신을 삭제해준다.
             Destroy(this.gameObject);
         }
 
